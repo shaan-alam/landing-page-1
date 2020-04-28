f174416c1450bcaf15f34fcbb83ad28a40a87cf8
Shaan Alam <shaanalam369@gmail.com>
2020-04-28 22:18:14 +0530

    completed the media queries

---
+++ js/main.js
@@ -0,0 +1,81 @@
+var sc = new ScrollReveal();
+sc.reveal("nav", {
+  origin: "top",
+  delay: 500,
+  distance: "15%",
+});
+
+sc.reveal(".narrow-container", {
+  origin: "top",
+  delay: 500,
+  distance: "15%",
+});
+
+sc.reveal(".girl", {
+  origin: "left",
+  delay: 500,
+  distance: "15%",
+});
+
+sc.reveal(".girl-div-text", {
+  origin: "right",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".partners-div", {
+  origin: "bottom",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".showcase", {
+  origin: "top",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".search .col-7", {
+  origin: "left",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".search .search-img", {
+  origin: "right",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".work-quick .col-5", {
+  origin: "left",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".work-quick .col-7", {
+  origin: "right",
+  delay: 520,
+  distance: "15%",
+});
+
+sc.reveal(".blue-ribbon", {
+  origin: "top",
+  delay: 520,
+  distance: "2%",
+});
+
+sc.reveal(".container .main-section", {
+  origin: "top",
+  delay: 520,
+  distance: "2%",
+});
+
+
+sc.reveal("footer", {
+  origin: "bottom",
+  delay: 520,
+  distance: "2%",
+});
+
+
